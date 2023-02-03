01ff0975bd6ba42d3f4f7b519766d53f73bfebc8
yihsiuelee <r07522802@ntu.edu.tw>
2023-02-03 19:40:21 +0800

    roughly finish todos

--- hw2/code/src/gcopter/include/gcopter/cubic_spline.hpp
+++ hw2/code/src/gcopter/include/gcopter/cubic_spline.hpp
@@ -184,32 +184,124 @@ namespace cubic_spline
         Eigen::Vector2d tailP;
         BandedSystem A;
         Eigen::MatrixX2d b;
+        Eigen::Matrix2Xd coeff;
 
     public:
         inline void setConditions(const Eigen::Vector2d &headPos,
                                   const Eigen::Vector2d &tailPos,
                                   const int &pieceNum)
         {
-           //TODO
+            // TODO
+            //  This is a function to set the coefficients matrix A (AD=x represent the curve)
+            //  which has no 'return'.
+            N = pieceNum;
+            headP = headPos;
+            tailP = tailPos;
+
+            // 建立矩阵
+            A.create(N - 1, 3, 3);
+            b.resize(N - 1, 2);
 
             return;
         }
 
         inline void setInnerPoints(const Eigen::Ref<const Eigen::Matrix2Xd> &inPs)
         {
-          //TODO
+            // TODO
+            //  This is a function to set the inner point x (AD=x) of the curve which
+            //  has no return.
+            A.reset();
+
+            for (int i = 1; i < N - 2; i++)
+            {
+                A(i, i - 1) = 1;
+                A(i, i) = 4;
+                A(i, i + 1) = 1;
+            }
+
+            A(0, 0) = 4;
+            A(0, 1) = 1;
+            A(N - 2, N - 3) = 1;
+            A(N - 2, N - 2) = 4;
+
+            b.setZero();
+
+            for (int i = 1; i < N - 2; ++i)
+            {
+                b.row(i) = 3 * (inPs.col(i + 1).transpose() - inPs.col(i - 1).transpose());
+            }
+
+            b.row(N - 2) = 3 * (tailP.transpose() - inPs.col(N - 3).transpose());
+            b.row(0) = 3 * (inPs.col(1).transpose() - headP.transpose());
+
+            A.factorizeLU();
+            A.solve(b);
+
+            // D 矩阵的 1 ～ N-1 行被存放在了 b 矩阵中
+            Eigen::MatrixXd D;
+            D.resize(N, 2);
+            D.setZero();
+
+            for (int i = 0; i < N - 1; ++i)
+            {
+                D.row(i + 1) = b.row(i);
+            }
+
+            // 系数矩阵 coeff = [ai, bi, ci, di];
+            coeff.resize(2, 4 * N);
+            coeff.setZero();
+
+            for (int i = 1; i < N - 1; ++i)
+            {
+                coeff.col(4 * i + 0) = inPs.col(i - 1);
+                coeff.col(4 * i + 1) = D.row(i).transpose();
+                coeff.col(4 * i + 2) = 3 * (inPs.col(i) - inPs.col(i - 1)) - 2 * D.row(i).transpose() - D.row(i + 1).transpose();
+                coeff.col(4 * i + 3) = 2 * (inPs.col(i - 1) - inPs.col(i)) + 1 * D.row(i).transpose() + D.row(i + 1).transpose();
+            }
+
+            coeff.col(0) = headP;
+            coeff.col(1) = Eigen::Vector2d::Zero();
+            coeff.col(2) = 3 * (inPs.col(0) - headP) - D.row(1).transpose();
+            coeff.col(3) = 2 * (headP - inPs.col(0)) + D.row(1).transpose();
+
+            coeff.col(4 * (N - 1) + 0) = inPs.col(N - 2);
+            coeff.col(4 * (N - 1) + 1) = D.row(N - 1).transpose();
+            coeff.col(4 * (N - 1) + 2) = 3 * (tailP - inPs.col(N - 2)) - 2 * D.row(N - 1).transpose();
+            coeff.col(4 * (N - 1) + 3) = 2 * (inPs.col(N - 2) - tailP) + 1 * D.row(N - 1).transpose();
+
+            b.resize(4 * N, 2);
+            b = coeff.transpose();
+
             return;
         }
 
         inline void getCurve(CubicCurve &curve) const
         {
-          //TODO
-           return;
+            // TODO
+            curve.clear();
+            curve.reserve(N);
+            for (int i = 0; i < N; ++i)
+            {
+                curve.emplace_back(1.0,
+                                   b.block<4, 2>(4 * i, 0)
+                                       .transpose()
+                                       .rowwise()
+                                       .reverse());
+            }
+            return;
         }
 
         inline void getStretchEnergy(double &energy) const
         {
-           //TODO
+            // TODO
+            //  An example for you to finish the other function
+            energy = 0.0;
+            for (int i = 0; i < N; ++i)
+            {
+                energy += 4.0 * b.row(4 * i + 2).squaredNorm() +
+                          12.0 * b.row(4 * i + 2).dot(b.row(4 * i + 3)) +
+                          12.0 * b.row(4 * i + 3).squaredNorm();
+            }
             return;
         }
 
@@ -220,7 +312,81 @@ namespace cubic_spline
 
         inline void getGrad(Eigen::Ref<Eigen::Matrix2Xd> gradByPoints) const
         {
-            //TODO
+            // TODO
+            //  Step 1 计算 dD/dx
+            Eigen::MatrixXd A_tmp;
+            Eigen::MatrixXd partial_B;
+
+            A_tmp.resize(N - 1, N - 1);
+            A_tmp.setZero();
+
+            partial_B.resize(N - 1, N - 1);
+            partial_B.setZero();
+
+            for (int i = 1; i < N - 2; i++)
+            {
+                A_tmp(i, i - 1) = 1;
+                A_tmp(i, i) = 4;
+                A_tmp(i, i + 1) = 1;
+                partial_B(i, i - 1) = -3;
+                partial_B(i, i + 1) = 3;
+            }
+
+            A_tmp(0, 0) = 4;
+            A_tmp(0, 1) = 1;
+            partial_B(0, 1) = 3;
+
+            A_tmp(N - 2, N - 3) = 1;
+            A_tmp(N - 2, N - 2) = 4;
+            partial_B(N - 2, N - 3) = -3;
+
+            // Eigen::MatrixXd A_inv = A_tmp.inverse();
+            Eigen::MatrixXd partial_D = A_tmp.inverse() * partial_B;
+
+            // * step2：获取 d(xi - xi+1)/dx
+            Eigen::MatrixXd partial_diff_x;
+            partial_diff_x.resize(N, N - 1);
+            partial_diff_x.setZero();
+            partial_diff_x(0, 0) = -1;
+            partial_diff_x(N - 1, N - 2) = 1;
+
+            for (int i = 1; i < N - 1; ++i)
+            {
+                partial_diff_x(i, i) = -1;
+                partial_diff_x(i, i - 1) = 1;
+            }
+
+            // Step 3: 获取 dc/dx & dd/dx
+            Eigen::MatrixXd partial_c;
+            partial_c.resize(N, N - 1);
+            partial_c.setZero();
+
+            Eigen::MatrixXd partial_d;
+            partial_d.resize(N, N - 1);
+            partial_d.setZero();
+
+            partial_c.row(0) = -3 * partial_diff_x.row(0) - partial_D.row(0);
+            partial_d.row(0) = 2 * partial_diff_x.row(0) + partial_D.row(0);
+
+            for (int i = 1; i < N - 1; ++i)
+            {
+                partial_c.row(i) = -3 * partial_diff_x.row(i) - 2 * partial_D.row(i - 1) - partial_D.row(i);
+                partial_d.row(i) = 2 * partial_diff_x.row(i) + partial_D.row(i - 1) + partial_D.row(i);
+            }
+
+            partial_c.row(N - 1) = -3 * partial_diff_x.row(N - 1) - 2 * partial_D.row(N - 2);
+            partial_d.row(N - 1) = 2 * partial_diff_x.row(N - 1) + partial_D.row(N - 2);
+
+            // Step 4: 填入 gradByPoints
+            gradByPoints.setZero();
+
+            for (int i = 0; i < N; ++i)
+            {
+                Eigen::Vector2d c_i = coeff.col(4 * i + 2);
+                Eigen::Vector2d d_i = coeff.col(4 * i + 3);
+
+                gradByPoints += (24 * d_i + 12 * c_i) * partial_d.row(i) + (12 * d_i + 8 * c_i) * partial_c.row(i);
+            }
         }
     };
 }
--- hw2/code/src/gcopter/include/gcopter/path_smoother.hpp
+++ hw2/code/src/gcopter/include/gcopter/path_smoother.hpp
@@ -34,7 +34,74 @@ namespace path_smoother
                                           const Eigen::VectorXd &x,
                                           Eigen::VectorXd &g)
         {
-           //TODO
+            // TODO
+            //  Hints:This is a function which define the costfunction, you can use
+            //  the content which *ptr point to initialize a quote of Class:
+            //  PathSmoother, the you can use the quote to visit the cubSpline
+            auto instance = reinterpret_cast<path_smoother::PathSmoother *>(ptr);
+            const int points_nums = instance->pieceN - 1;
+            Eigen::Matrix2Xd grad;
+
+            grad.resize(2, points_nums);
+            grad.setZero();
+
+            double cost = 0.0;
+
+            // Step 1: 送入目标点，计算曲线系数
+            Eigen::Matrix2Xd inPs;
+            inPs.resize(2, points_nums);
+            inPs.row(0) = x.head(points_nums);
+            inPs.row(1) = x.tail(points_nums);
+            instance->cubSpline.setInnerPoints(inPs);
+
+            // Step 2: 计算能量函数值和梯度
+            double energy = 0.0;
+            Eigen::Matrix2Xd energy_grad;
+            energy_grad.resize(2, points_nums);
+            energy_grad.setZero();
+            instance->cubSpline.getStretchEnergy(energy);
+            instance->cubSpline.getGrad(energy_grad);
+
+            cost += energy;
+            grad += energy_grad;
+
+            // Step3: 计算碰撞函数和代价
+            double obstacles = 0.0;
+            Eigen::Matrix2Xd potential_grad;
+            potential_grad.resize(2, points_nums);
+            potential_grad.setZero();
+
+            for (int i = 0; i < points_nums; ++i)
+            {
+                for (int j = 0; j < instance->diskObstacles.cols(); ++j)
+                {
+                    Eigen::Vector2d diff = inPs.col(i) - instance->diskObstacles.col(j).head(2);
+                    double distance = diff.norm();
+                    double delta = instance->diskObstacles(2, j) - distance;
+
+                    if (delta > 0.0)
+                    {
+                        obstacles += instance->penaltyWeight * delta;
+                        potential_grad.col(i) += instance->penaltyWeight * (-diff / distance);
+                    }
+                }
+            }
+            cost += obstacles;
+            grad += potential_grad;
+
+            // Step4: 得到结果
+            g.setZero();
+            g.head(points_nums) = grad.row(0).transpose();
+            g.tail(points_nums) = grad.row(1).transpose();
+
+            // std::cout << std::setprecision(10)
+            std::cout << "------------------------"
+                      << "\n";
+            std::cout << "Function Value: " << cost << "\n";
+            std::cout << "Gradient Inf Norm: " << g.cwiseAbs().maxCoeff() << "\n";
+            std::cout << "------------------------"
+                      << "\n";
+
             return cost;
         }
 
@@ -63,7 +130,39 @@ namespace path_smoother
                                const Eigen::Matrix2Xd &iniInPs,
                                const double &relCostTol)
         {
-          //TODO
+            // TODO
+            Eigen::VectorXd x(pieceN * 2 - 2);
+            Eigen::Map<Eigen::Matrix2Xd> innerP(x.data(), 2, pieceN - 1);
+            innerP = iniInPs;
+
+            double minCost = 0.0;
+            lbfgs_params.mem_size = 64;
+            lbfgs_params.past = 3;
+            lbfgs_params.min_step = 1.0e-32;
+            lbfgs_params.g_epsilon = 1.0e-6;
+            lbfgs_params.delta = relCostTol;
+
+            int ret = lbfgs::lbfgs_optimize(x,
+                                            minCost,
+                                            &PathSmoother::costFunction,
+                                            nullptr,
+                                            this,
+                                            lbfgs_params);
+
+            if (ret >= 0)
+            {
+                // cubSpline.setInnerPoints(innerP);
+                cubSpline.getCurve(curve);
+            }
+            else
+            {
+                curve.clear();
+                minCost = INFINITY;
+                std::cout << "Optimization Failed: "
+                          << lbfgs::lbfgs_strerror(ret)
+                          << std::endl;
+            }
+
             return minCost;
         }
     };
